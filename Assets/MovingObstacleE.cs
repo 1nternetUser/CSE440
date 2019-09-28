2de88c028e370c70d3a7b80a507a9be3c62486ba
1nternetUser <43967720+1nternetUser@users.noreply.github.com>
2019-09-28 08:25:14 -0700

    Add files via upload
    9/28/19 08:24 AM 
    I spent all night on this assignment because my laptop is having constant updates where it keeps resetting my laptop. Thus, if I shut it down and update it might erase my progress, so I'm going to send all my files in case if it does happen. I won't be back home until late night.

---
+++ Assets/MovingObstacleE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObstacleE : MonoBehaviour
+{
+    public float speedMultiplier = 5.0f;
+    public float length = 15.0f;
+    public float pointX;
+
+    private Rigidbody2D rb;
+
+
+    private bool PathFinderL = false;
+
+    //Start is called before the first frame update
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 point = new Vector3(pointX + Mathf.PingPong(speedMultiplier * Time.time, length), 20, 0);
+        transform.position = point;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float dirX;
+
+    [SerializeField]
+
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode jump = KeyCode.Space;
+
+    public float speedMultiplier = 5.0f;
+    public float jumpPower = 15.0f;
+    public Transform groundDetection;
+    public LayerMask ground;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    private bool x2Jump = false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newMovement;
+
+        isGrounded = Physics2D.OverlapCircle(groundDetection.position, 0.1f, ground);
+
+        Debug.Log("isGrounded: " + isGrounded);
+
+        if (Input.GetKey(moveLeft)) //Movement Speed Left
+        {
+            newMovement = new Vector2(rb.position.x - (Time.deltaTime * speedMultiplier), rb.position.y);
+            rb.position = newMovement;
+        }
+
+        if (Input.GetKey(moveRight)) //Movement Speed Right
+        {
+            newMovement = new Vector2(rb.position.x + (Time.deltaTime * speedMultiplier), rb.position.y);
+            rb.position = newMovement;
+        }
+
+        if (rb.velocity.y == 0)
+        {
+            isGrounded = true;
+        }
+        else isGrounded = false;
+
+        if (isGrounded)
+        {
+            x2Jump = true;
+        }
+
+        if (isGrounded && Input.GetKeyDown(jump)) //Detects if you're in the ground and are you pressing the jump key?
+        {
+            Debug.Log("I am jumping.");
+            Jumping();
+        }
+        else if (x2Jump && Input.GetKeyDown(jump))
+        {
+            Jumping();
+            Debug.Log("I am Double Jumping.");
+            x2Jump = false;
+        }
+        dirX = Input.GetAxis("Horizontal") * speedMultiplier;
+    }
+
+    private void FixedUpdate() //Jumping Mechanic
+    {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    void Jumping()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+}
